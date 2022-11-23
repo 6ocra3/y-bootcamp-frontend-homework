b685b9dbb9a867945a33645d6fd3abd024b94bff
6ocra3 <6o3kmm@mail.ru>
2022-11-23 21:25:52 +0300

    delete last homework

--- 5-tests/test/hermione/example.hermione.js
+++
@@ -1,9 +0,0 @@
-const { assert } = require('chai');
-
-describe('github', async function () {
-    it('Тест, который пройдет', async function ({ browser }) {
-        await this.browser.url("/hw/store")
-        // const home = await this.browser.$(".Home")
-        // await home.waitForExist()
-    });
-});
